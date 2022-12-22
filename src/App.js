ceebac198d42c9d971600dafaffb44baff17a940
NayChi-Cloud <onyxjay1728@gmail.com>
2022-12-22 19:28:27 +0630

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Feature from './components/Feature';
+import Signup from './components/Signup';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Feature />
+    <Signup /> 
+    <Footer />
+    </>
+  );
+}
+
+export default App;
